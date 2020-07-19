f91f5f1cc601c2bd78e97d84f7bd370b513b69a8
Nick Kniola <nickkniola@me.com>
2020-07-19 17:40:36 -0500

    Navbar icons, changes

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Media } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
@@ -22,28 +22,32 @@ class Header extends Component {
         return (
             <React.Fragment>
                 <Navbar dark sticky="top" expand="md">
-                    <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
-                        <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/home">
-                                        Home
+                    <NavbarToggler onClick={this.toggleNav} />
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/home">
+                                    Home
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/pairing">
-                                        Pairing
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/pairing">
+                                    Pairing
                                     </NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink className="nav-link" to="/suggest">
-                                        Suggestion
+                            </NavItem>
+                            <NavItem>
+                                <NavLink className="nav-link" to="/suggest">
+                                    Suggestion
                                     </NavLink>
-                                </NavItem>
-                            </Nav>
-                        </Collapse>
-                    </div>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                    <NavbarBrand className="text-center" href="/">
+                        Hang
+                    </NavbarBrand>
+                    <NavLink className="nav-link" to="/"><i className="fa fa-android  fa-fw fa-lg" ></i></NavLink>
+                    <NavLink className="nav-link" to="/"><i className="fa fa-apple fa-fw fa-lg"></i></NavLink>
+                    <NavLink className="nav-link" to="/"><i className="fa fa-envelope fa-fw fa-lg"></i></NavLink>
                 </Navbar>
             </React.Fragment>
         );

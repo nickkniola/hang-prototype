862fcc83597a79d33fcea4f192d1f6eff6342ddf
Nick Kniola <nickkniola@me.com>
2020-08-18 20:48:13 -0500

    Updates

---
+++ precache-manifest.0b358ea132fa2315bafc399b1dcc1ed3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea4049f231e5dcdb6c3108c134f2bcc",
+    "url": "/nickkniola/hang/index.html"
+  },
+  {
+    "revision": "1cbe0f0bab8914246444",
+    "url": "/nickkniola/hang/static/css/2.3ca71447.chunk.css"
+  },
+  {
+    "revision": "c3f6d879e58824597fff",
+    "url": "/nickkniola/hang/static/css/main.d2215518.chunk.css"
+  },
+  {
+    "revision": "1cbe0f0bab8914246444",
+    "url": "/nickkniola/hang/static/js/2.08aef468.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nickkniola/hang/static/js/2.08aef468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3f6d879e58824597fff",
+    "url": "/nickkniola/hang/static/js/main.42ae6a25.chunk.js"
+  },
+  {
+    "revision": "53bc2d2c7e2e90a9973f",
+    "url": "/nickkniola/hang/static/js/runtime-main.dddb6c46.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/nickkniola/hang/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/nickkniola/hang/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/nickkniola/hang/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nickkniola/hang/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/nickkniola/hang/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);

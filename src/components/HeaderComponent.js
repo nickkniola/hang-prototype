a2502a45ddd22224fb6f059208fa2fb570a83a9e
Nick Kniola <nickkniola@me.com>
2020-08-20 17:27:35 -0500

    Removed unnecessary code

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -33,7 +33,7 @@ function Header(props) {
                         </NavItem>
                     </Nav>
                     <NavbarBrand className="navbar-brand d-none d-md-block" href="/">
-                        <img src="/assets/images/hang_logo.png" height="30" width="30" alt="Hang Logo" />Hang
+                        <img src="assets/images/hang_logo.png" height="30" width="30" alt="Hang Logo" />Hang
                     </NavbarBrand>
                     <div className="ml-auto xs={mx-auto}">
                         <a className="nav-link-icon" href="/"><i className="fa fa-android  fa-fw fa-lg" ></i></a>
@@ -42,7 +42,7 @@ function Header(props) {
                     </div>
                 </Collapse>
                 <NavbarBrand className="navbar-brand text-white d-block d-md-none mx-auto pr-4" href="/">
-                    <img src="/assets/images/hang_logo.png" height="30" width="30" alt="Hang Logo" />Hang
+                    <img src="assets/images/hang_logo.png" height="30" width="30" alt="Hang Logo" />Hang
                 </NavbarBrand>
             </Navbar>
         </React.Fragment>
--- src/shared/userData.js
+++ src/shared/userData.js
@@ -6,28 +6,28 @@ export const USERDATA = [
         name: "Nick",
         activity: "Movie",
         location: "Los Angeles",
-        image: "/assets/images/nick.jpg"
+        image: "public/assets/images/nick.jpg"
     },
     {
         id: 1,
         name: "Tom",
         activity: "Sports",
         location: "Chicago",
-        image: "/assets/images/tom.jpg"
+        image: "assets/images/tom.jpg"
     },
     {
         id: 2,
         name: "Kathy",
         activity: "Museum",
         location: "Paris",
-        image: "/assets/images/kathy.jpg"
+        image: "assets/images/kathy.jpg"
     },
     {
         id: 3,
         name: "Ali",
         activity: "Food",
         location: "Atlanta",
-        image: "/assets/images/ali.jpg"
+        image: "assets/images/ali.jpg"
     }
 ];
 
